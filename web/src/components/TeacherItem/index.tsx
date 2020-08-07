eb182bba4f2ef8a5bdef15afbc5c68c6797b4357
walysonfelipe <35854466+walysonfelipe@users.noreply.github.com>
2020-08-07 14:24:43 -0300

    finalized adjustments

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,36 +3,59 @@ import React from 'react';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher{
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: number;  
+}
+
+ interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({ teacher}) => {
+    function createNewConnection () {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
                 <img
-                    src="https://avatars2.githubusercontent.com/u/35854466?s=460&u=4694b2ee016f40eff0bd4fb874ed828dfc3de8c6&v=4"
-                    alt="Walyson Felipe"
+                    src= {teacher.avatar}
+                     alt={teacher.name}
                 />
                 <div>
-                    <strong>Walyson Felipe</strong>
-                    <span>Biologia</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br /><br />
-                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
+            {teacher.bio}
+           </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 70,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    target="_blank" rel="noopener noreferrer"
+                    onClick={createNewConnection}>
+            
                     <img src={whatsAppIcon} alt="Whats App"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
